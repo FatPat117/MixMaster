ce9986e7a63c38734336849d4473a5099a65f7b5
Phat117 <ntphat110705@gmail.com>
2025-06-22 21:22:32 +0700

    CocktailCard

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
+import CocktailCard from "./CockTailCard";
+
 const Wrapper = styled.div`
         display: grid;
         grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
         gap: 2rem;
 `;
+
 export default function CocktailList({ drinks }) {
         if (drinks.length < 1) {
                 return <h4 style={{ textAlign: "center" }}>No Matching Cocktails Found...</h4>;
