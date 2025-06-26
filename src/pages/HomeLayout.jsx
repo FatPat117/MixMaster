1a3bc5b26328883e1ab4868e728d160aa6d558e0
Phat117 <ntphat110705@gmail.com>
2025-06-26 15:00:48 +0700

    Global Loading and Context

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 export default function HomeLayout() {
+        const navigation = useNavigation();
+        const isPageLoading = navigation.state === "loading";
+
         return (
                 <>
                         <Navbar />
                         <section className="page">
-                                <Outlet />
+                                {isPageLoading ? <div className="loading"></div> : <Outlet />}
                         </section>
                 </>
         );
